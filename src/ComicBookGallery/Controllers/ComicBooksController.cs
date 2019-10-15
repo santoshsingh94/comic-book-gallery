d1f6596250f6e2e63f246a936c95ec69eaf199fc
Santosh Singh <ausantoshsingh@gmail.com>
2019-10-15 13:43:39 +0530

    Added comic book details view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,13 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Details()
         {
-            // action result type provided by Mvc.
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");     //If today is Tuesday then jump automaticaly to home page.
-            }
-            return Content("Hello from the comic book");          //Content result is an action result type provided by Mvc.
-            
+            return View();
         }
     }
 }
