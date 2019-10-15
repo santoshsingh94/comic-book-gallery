048a13318b73c45ffaf2713a64d8003359546afc
Santosh Singh <ausantoshsingh@gmail.com>
2019-10-15 15:51:01 +0530

    Updated comic book detail veiw

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,10 +6,22 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBooksController : Controller 
+    public class ComicBooksController : Controller
     {
         public ActionResult Details()
         {
+            ViewBag.SeriesTitle = "The Amazing Spider-man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of doctor octopus life and his one, last , great act of revange!" +
+                        "Even if Spider ma  n survives...<strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script:Dan Slott",
+                "Pencils : Humberto Ramos",
+                "Inks :Victor Olazaba",
+                "Colors : Edger Delgado",
+                "Letters : Chris Eliopoulos"
+            };
             return View();
         }
     }
