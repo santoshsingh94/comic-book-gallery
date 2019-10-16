21b3b5752df1d73276616610f136953576cfe59a
Santosh Singh <ausantoshsingh@gmail.com>
2019-10-16 14:06:54 +0530

    Updated comicBooks controller to use repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,18 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Details()
+        private ComicBookRepository _comicBookRepository = null;
+        public ComicBooksController()
         {
-            
+            _comicBookRepository = new ComicBookRepository();
+        }
+        public ActionResult Details(int? id)   //Nullable type parameter . So that if no argument passed then there should not be any error thrown.
+        {
+            if (id==null)
+            {
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
             return View(comicBook);     //making strongly typed views
             
         }
